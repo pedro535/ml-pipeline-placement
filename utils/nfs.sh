0898a5f035c25767a4b268426e645f094e15facb
Pedro Rodrigues <pedrofrodrigues4@gmail.com>
2025-02-17 10:53:56 +0000

    Update data script to use ssh to copy files

--- utils/nfs.sh
+++ utils/nfs.sh
@@ -1,19 +1,52 @@
 #!/bin/bash
 
+server_ip="10.255.32.122"
+server_user="atnoguser"
+base_dir="/mnt/datasets"
+
 option=$1
 dataset_path=$2
-base_dir="/mnt/datasets"
-namespace="data-registry"
-linked_pod=$(kubectl get pods -n $namespace --no-headers | awk '{print $1}')
+dataset_name=$(basename $dataset_path)
+datasets_dir=$(dirname $dataset_path)
+
+
+function upload {
+    echo "Uploading dataset to remote NFS Server..."
+    scp -r $datasets_dir/$dataset_name.zip $server_user@$server_ip:/tmp
+
+    echo "Unzipping dataset..."
+    ssh $server_user@$server_ip "sudo unzip /tmp/$dataset_name.zip -d $base_dir; sudo rm /tmp/$dataset_name.zip"
+
+    echo "Dataset uploaded successfully"
+    rm $datasets_dir/$dataset_name.zip
+}
+
+function remove {
+    echo "Removing dataset $dataset_path from remote NFS Server..."
+    ssh $server_user@$server_ip "sudo rm -rf $base_dir/$dataset_name"
+    echo "Dataset removed successfully"
+}
+
+function zip_dataset {
+    echo "Zipping dataset $dataset_path..."
+    cd $datasets_dir
+    zip -r $dataset_name.zip $dataset_name
+    echo "Dataset zipped successfully"
+    cd ..
+}
+
+function delete_hidden_files {
+    find $dataset_path -name ".*" -exec rm -rf {} \;
+    echo "Hidden files deleted successfully"
+}
+
 
 if [ $option == "upload" ]; then
-    echo "Uploading dataset $dataset_path to remote NFS Server..."
-    kubectl cp $dataset_path $namespace/$linked_pod:$base_dir
-    echo "Dataset uploaded successfully"
+    delete_hidden_files
+    zip_dataset
+    upload
 elif [ $option == "rm" ]; then
-    echo "Removing dataset $dataset_path from remote NFS Server..."
-    kubectl exec -n $namespace $linked_pod -- rm -rf $base_dir/$dataset_path
-    echo "Dataset removed successfully"
+    remove
 else
     echo "Invalid option. Please use 'upload' or 'download'"
 fi
