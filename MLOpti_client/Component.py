d1182af010da09b35046539e5c04a06155efcf70
Pedro Rodrigues <pedrofrodrigues4@gmail.com>
2025-03-05 21:40:38 +0000

    Add pipeline and component class for client package

--- MLOpti_client/Component.py
+++ MLOpti_client/Component.py
@@ -1,14 +1,4 @@
-import ast
-import astor
 import inspect
-import black
-
-from MLOpti_client.consts import (
-    IMPORTS_MAPPING,
-    TYPES_MAPPING,
-    KFP_COMPONENT_DECORATOR
-)
-
 
 class Component:
 
@@ -18,38 +8,20 @@ class Component:
         self.user_args = args
         self.name = func.__name__
         self.file = None
-        self.tree = None
-        self.arg_types = {}
+        self.filename = None
         self.volumes = []
-
-        self.get_source_file()
-        self.get_tree()
-        self.get_arg_types()
+        self._get_source_file()
         
 
-    def get_source_file(self):
+    def _get_source_file(self):
         """
         Get the source file of the function
         """
         self.file = inspect.getfile(self.func)
-
-
-    def get_tree(self):
-        """
-        Parse the source file to an AST
-        """
-        with open(self.file, "r") as f:
-            tree = ast.parse(f.read())
-        tree.body = tree.body[2:]         # Remove sys.path.append used for development
-        self.tree = tree
-
-
-    def get_arg_types(self):
-        """
-        Get all arguments of the function
-        """
-        for arg_name, arg_type in self.func.__annotations__.items():
-            self.arg_types[arg_name] = arg_type.__name__
+        self.filename = self.file.split("/")[-1]
+        
+        if self.filename.split(".")[0] != self.name:
+            raise ValueError("The file name must match the function name")
 
 
     def mount_volume(self, pvc, mount_path):
@@ -57,101 +29,3 @@ class Component:
         Mount a volume to a component
         """
         self.volumes.append((pvc, mount_path))
-
-    
-    def remove_type_imports(self):
-        """
-        Remove marked types imports
-        """
-        import_nodes = []
-        for node in ast.walk(self.tree):
-            if isinstance(node, ast.ImportFrom) and "artifacts" in node.module:
-                import_nodes.append(node)
-        for node in import_nodes:
-            self.tree.body.remove(node)
-
-    
-    def get_imports(self):
-        """
-        Get modules and names to import
-        """
-        imports = {}
-        for arg_type in self.arg_types.values():
-            if arg_type not in IMPORTS_MAPPING:
-                continue
-            module, names = IMPORTS_MAPPING[arg_type]
-            if module not in imports:
-                imports[module] = set()
-            imports[module].update(names)
-        return imports
-
-
-    def add_imports(self):
-        """
-        Add import statements to the top of the file
-        """
-        imports = self.get_imports()
-        for module, names in imports.items():
-            node = ast.ImportFrom(
-                module=module,
-                names=[ast.alias(name=name, asname=None) for name in names],
-                level=0,
-            )
-            self.tree.body.insert(0, node)
-
-        # Import @dsl.component decorator
-        module, names = IMPORTS_MAPPING[KFP_COMPONENT_DECORATOR]
-        node = ast.ImportFrom(
-            module=module,
-            names=[ast.alias(name=name, asname=None) for name in names],
-            level=0,
-        )
-        self.tree.body.insert(0, node)
-
-
-    def add_decorator(self):
-        """
-        Add the kfp component decorator to the function
-        """
-        for node in ast.walk(self.tree):
-            if isinstance(node, ast.FunctionDef) and node.name == self.name:
-                decorator_node = ast.Call(
-                    func=ast.Name(id=KFP_COMPONENT_DECORATOR, ctx=ast.Load()),
-                    args=[],
-                    keywords=[
-                        ast.keyword(arg="base_image", value=ast.Constant(s=self.image))
-                    ],
-                )
-                node.decorator_list.append(decorator_node)
-                break
-
-
-    def update_arg_types(self):
-        """
-        Replace artifact marker types with kfp data types
-        """
-        for node in ast.walk(self.tree):
-            if isinstance(node, ast.FunctionDef) and node.name == self.name:
-                for arg in node.args.args:
-                    arg_type = arg.annotation.id
-                    if arg_type in TYPES_MAPPING:
-                        arg.annotation.id = TYPES_MAPPING[arg_type]
-                break
-
-
-    def compile(self) -> str:
-        """
-        Compile the component to a kfp component
-        """
-        # Apply transformations
-        self.remove_type_imports()
-        self.add_imports()
-        self.add_decorator()
-        self.update_arg_types()
-
-        # Parse and save modified code
-        ast.fix_missing_locations(self.tree)
-        kfp_component = astor.to_source(self.tree)
-        kfp_component = black.format_str(kfp_component, mode=black.Mode())
-        
-        return kfp_component
--- MLOpti_client/Pipeline.py
+++ MLOpti_client/Pipeline.py
@@ -1,29 +1,13 @@
-from pathlib import Path
-import uuid
-import ast
-import astor
-import black
+import requests
 from typing import List
-
+import inspect
 from MLOpti_client import Component
-from MLOpti_client.consts import (
-    IMPORTS_MAPPING,
-    KFP_PIPELINE_DECORATOR,
-    PIPELINE_IMPORTS
-)
-
-# TODO: pass the following vars as arguments
-kfp_host = "http://localhost:3000"
-enable_caching = False
 
 class Pipeline:
 
     def __init__(self, name):
         self.name = name
-        self.func_name = name.replace(" ", "_").lower()
         self.components = []
-        self.artifacts = {}  # {output_artifact_name: producer_component_name}
-        self.tree = None
 
 
     def add(self, components: List[Component]):
@@ -31,231 +15,45 @@ class Pipeline:
         Add a list of components to the pipeline
         """
         self.components.extend(components)
-        for component in components:
-            for arg_name, arg_type in component.arg_types.items():
-                if "Output" in arg_type:
-                    self.artifacts[arg_name] = component.name
 
 
-    def compile_components(self, pipeline_id):
+    def submit(self, server):
         """
-        Compile each component to equivalent kfp component
+        Submit the pipeline to the server
         """
-        for component in self.components:
-            kfp_component = component.compile()
-            with open(f"{pipeline_id}/{component.name}.py", "w") as f:
-                f.write(kfp_component)
+        files = self._prepare_files()
+        response = self._send_request(server, files)
+        self._handle_response(response)
 
 
-    def add_imports(self):
+    def _prepare_files(self):
         """
-        Add imports to the pipeline file
+        Prepare the files for submission
         """
-        imports = {}
-        for name in PIPELINE_IMPORTS:
-            module, names = IMPORTS_MAPPING[name]
-            imports[module] = imports.get(module, []) + names
-        
-        for module, names in imports.items():
-            node = ast.ImportFrom(
-                module=module,
-                names=[ast.alias(name=name, asname=None) for name in names],
-                level=0,
-            )
-            self.tree.body.append(node)
-
-        # Compiled components
-        for component in self.components:
-            node = ast.ImportFrom(
-                module=component.name,
-                names=[ast.alias(name=component.name, asname=None)],
-                level=0,
-            )
-            self.tree.body.append(node)
+        files = [("files", (c.filename, open(c.file, "rb"))) for c in self.components]
+        files.append(("files", ("pipeline.py", open("pipeline.py", "rb"))))
+        return files
 
 
-    def create_function(self):
+    def _send_request(self, server, files):
         """
-        Create the pipeline function
+        Send the POST request to the server
         """
-        node = ast.FunctionDef(
-            name=self.func_name,
-            args=ast.arguments(
-                args=[],
-                defaults=[],
-            ),
-            body=[]
-        )
-        return node
+        try:
+            response = requests.post(f"{server}/submit/", files=files)
+            response.raise_for_status()
+            return response
+        except requests.RequestException as e:
+            print(f"An error occurred: {e}")
+            return None
 
 
-    def add_decorator(self, func_node: ast.FunctionDef):
+    def _handle_response(self, response):
         """
-        Add the kfp pipeline decorator to the function
+        Handle the server response
         """
-        func_node.decorator_list = [
-            ast.Call(
-                func=ast.Name(id=KFP_PIPELINE_DECORATOR, ctx=ast.Load()),
-                args=[],
-                keywords=[
-                    ast.keyword(arg="name", value=ast.Constant(s=self.name))
-                ],
-            )
-        ]
-
-
-    def mount_volumes(self, component, func_node):
-        """
-        Mount volumes to the component
-        """
-        for pvc, mount_path in component.volumes:
-            invoke_node = ast.Call(
-                func=ast.Name(id="mount_pvc", ctx=ast.Load()),
-                args=[],
-                keywords=[
-                    ast.keyword(
-                        arg="task",
-                        value=ast.Name(id=f"{component.name}_task", ctx=ast.Load())
-                    ),
-                    ast.keyword(
-                        arg="pvc_name",
-                        value=ast.Constant(value=pvc)
-                    ),
-                    ast.keyword(
-                        arg="mount_path",
-                        value=ast.Constant(value=mount_path)
-                    )
-                ],
-            )
-            node = ast.Assign(
-                targets=[ast.Name(id=f"{component.name}_task", ctx=ast.Store())],
-                value=invoke_node,
-            )
-            func_node.body.append(node)
-
-
-    def call_components(self, func_node):
-        """
-        Call the compiled components in the pipeline function
-        """
-        for component in self.components:
-            args = {}
-            for arg_name, arg_type in component.arg_types.items():
-                if arg_name in component.user_args:
-                    args[arg_name] = ast.Constant(component.user_args[arg_name])
-                elif arg_type.startswith("Input"):
-                    args[arg_name] = ast.Subscript(
-                        value=ast.Attribute(
-                            value=ast.Name(id=f"{self.artifacts[arg_name]}_task", ctx=ast.Load()),
-                            attr="outputs",
-                            ctx=ast.Load(),
-                        ),
-                        slice=ast.Constant(value=arg_name),
-                        ctx=ast.Load(),
-                    )
-            
-            node = ast.Assign(
-                targets=[ast.Name(id=f"{component.name}_task", ctx=ast.Store())],
-                value=ast.Call(
-                    func=ast.Name(id=component.name, ctx=ast.Load()),
-                    args=[],
-                    keywords=[
-                        ast.keyword(arg=k, value=v) for k, v in args.items()
-                    ],
-                ),
-            )
-            func_node.body.append(node)
-            self.mount_volumes(component, func_node)
-
-    
-    def create_client(self):
-        """
-        Create the kfp client
-        """
-        node = ast.Assign(
-            targets=[ast.Name(id="client", ctx=ast.Store())],
-            value=ast.Call(
-                func=ast.Name(id="Client", ctx=ast.Load()),
-                args=[],
-                keywords=[
-                    ast.keyword(arg="host", value=ast.Constant(value=kfp_host))
-                ],
-            ),
-        )
-        self.tree.body.append(node)
-
-
-    def add_create_run(self):
-        """
-        Call the kfp create run function to run the created pipeline
-        """
-        node = ast.Assign(
-            targets=[ast.Name(id="run", ctx=ast.Store())],
-            value=ast.Call(
-                func=ast.Attribute(
-                    value=ast.Name(id="client", ctx=ast.Load()),
-                    attr="create_run_from_pipeline_func",
-                    ctx=ast.Load(),
-                ),
-                args=[],
-                keywords=[
-                    ast.keyword(
-                        arg="pipeline_func",
-                        value=ast.Name(id=self.func_name, ctx=ast.Load())
-                    ),
-                    ast.keyword(
-                        arg="enable_caching",
-                        value=ast.Constant(value=enable_caching)
-                    )
-                ],
-            ),
-        )
-        print_node = ast.Expr(
-            value=ast.Call(
-                func=ast.Name(id="print", ctx=ast.Load()),
-                args=[
-                    ast.Constant(value="Run ID: "),
-                    ast.Attribute(
-                        value=ast.Name(id="run", ctx=ast.Load()),
-                        attr="run_id",
-                        ctx=ast.Load()
-                    )
-                ],
-                keywords=[],
-            )
-        )
-        self.tree.body.extend([node, print_node])
-
-
-    def build_pipeline(self, pipeline_id):
-        """
-        Build the kfp pipeline using the compiled components
-        """
-        self.tree = ast.Module(body=[])
-        self.add_imports()
-        func_node = self.create_function()
-        self.add_decorator(func_node)
-        self.call_components(func_node)
-        self.tree.body.append(func_node)
-        self.create_client()
-        self.add_create_run()
-
-        with open(f"{pipeline_id}/pipeline.py", "w") as f:
-            ast.fix_missing_locations(self.tree)
-            kfp_pipeline = astor.to_source(self.tree)
-            kfp_pipeline = black.format_str(kfp_pipeline, mode=black.Mode())
-            f.write(kfp_pipeline)
-
-
-    def run(self):
-        """
-        Run the pipeline using the kfp backend
-        """
-        pipeline_id = str(uuid.uuid4())
-        p = Path(f"{pipeline_id}/")
-        p.mkdir(parents=True, exist_ok=True)
-        
-        self.compile_components(pipeline_id)
-        self.build_pipeline(pipeline_id)
-
-        # TODO: execute the built pipeline
+        if response:
+            try:
+                print(response.json())
+            except ValueError:
+                print("Failed to parse response JSON")
