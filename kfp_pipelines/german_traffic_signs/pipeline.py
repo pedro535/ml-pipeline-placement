25d64c219c20efd234def26d96dfee1d3373a054
Pedro Rodrigues <pedrofrodrigues4@gmail.com>
2025-03-16 22:57:30 +0000

    Update gts original kfp pipeline

--- kfp_pipelines/german_traffic_signs/pipeline.py
+++ kfp_pipelines/german_traffic_signs/pipeline.py
@@ -1,11 +1,13 @@
-from kfp import dsl, compiler
-from kfp.kubernetes import mount_pvc
+from kfp import dsl, Client
+from kfp.kubernetes import mount_pvc, add_node_selector, add_toleration
 from typing import Dict
 
-from components.data_collection import data_collection
-from components.model_training import model_training
-from components.model_evaluation import model_evaluation
+from data_collection import data_collection
+from model_training import model_training
+from model_evaluation import model_evaluation
 
+
+KFP_URL = "http://localhost:3000"
 PVC = "datasets-pvc"
 MOUNT_PATH = "/mnt/datasets"
 
@@ -21,34 +23,91 @@ def gts_pipeline(
 ) -> Dict[str, float]:
 
     # 1. Data Collection
-    data_collection_op = data_collection(
+    data_collection_task = data_collection(
         dataset_path = dataset_path
     )
 
-    data_collection_op = mount_pvc(
-        task = data_collection_op,
-        pvc_name = PVC,
-        mount_path = MOUNT_PATH
-    )
-
     # 2. Model Training
-    model_training_op = model_training(
-        X_train_ds = data_collection_op.outputs["X_train_ds"],
-        X_val_ds = data_collection_op.outputs["X_val_ds"],
-        y_train_ds = data_collection_op.outputs["y_train_ds"],
-        y_val_ds = data_collection_op.outputs["y_val_ds"],
-        epochs = epochs,
+    model_training_task = model_training(
+        X_train_ds=data_collection_task.outputs["X_train_ds"],
+        X_val_ds=data_collection_task.outputs["X_val_ds"],
+        y_train_ds=data_collection_task.outputs["y_train_ds"],
+        y_val_ds=data_collection_task.outputs["y_val_ds"],
+        epochs=epochs,
         batch_size = batch_size
     )
 
     # 3. Model Evaluation
-    model_evaluation_op = model_evaluation(
-        model_artifact = model_training_op.outputs["model_artifact"],
-        X_test_ds = data_collection_op.outputs["X_test_ds"],
-        y_test_ds = data_collection_op.outputs["y_test_ds"]
+    model_evaluation_task = model_evaluation(
+        model_artifact=model_training_task.outputs["model_artifact"],
+        X_test_ds=data_collection_task.outputs["X_test_ds"],
+        y_test_ds=data_collection_task.outputs["y_test_ds"]
     )
 
-    return model_evaluation_op.output
+    # Mount volumes
+    data_collection_task = mount_pvc(
+        task=data_collection_task,
+        pvc_name=PVC,
+        mount_path=MOUNT_PATH
+    )
+
+    # Add node selectors
+    data_collection_task = add_node_selector(
+        task=data_collection_task,
+        label_key="kubernetes.io/hostname",
+        label_value="k3s-node1"
+    )
+
+    model_training_task = add_node_selector(
+        task=model_training_task,
+        label_key="kubernetes.io/hostname",
+        label_value="k3s-node2"
+    )
+
+    model_evaluation_task = add_node_selector(
+        task=model_evaluation_task,
+        label_key="kubernetes.io/hostname",
+        label_value="k3s-node3"
+    )
+
+    # Add tolerations
+    data_collection_task = add_toleration(
+        task=data_collection_task,
+        key="component-id",
+        operator="Equal",
+        value="data-collection",
+        effect="NoSchedule"
+    )
+
+    model_training_task = add_toleration(
+        task=model_training_task,
+        key="component-id",
+        operator="Equal",
+        value="model-training",
+        effect="NoSchedule"
+    )
+
+    model_evaluation_task = add_toleration(
+        task=model_evaluation_task,
+        key="component-id",
+        operator="Equal",
+        value="model-evaluation",
+        effect="NoSchedule"
+    )
 
 
-compiler.Compiler().compile(gts_pipeline, "pipeline.yaml")
+    return model_evaluation_task.output
+
+
+
+if __name__ == "__main__":
+    client = Client(host=KFP_URL)
+
+    client.create_run_from_pipeline_func(
+        pipeline_func=gts_pipeline,
+        arguments={
+            "dataset_path": "/mnt/datasets/german_traffic_signs",
+            "epochs": 1,
+            "batch_size": 128
+        }
+    )
