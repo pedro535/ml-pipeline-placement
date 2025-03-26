79c64f3eafadbe17039a2099d553778c1b57540e
Pedro Rodrigues <pedrofrodrigues4@gmail.com>
2025-03-26 15:32:40 +0000

    Add custom pipelines for UNSW-NB15

---
+++ pipelines/UNSW_NB15_SVM/model_training.py
@@ -0,0 +1,27 @@
+from mlopx.artifacts import InputDataset, OutputModel
+
+
+def model_training(
+    x_train_ds: InputDataset,
+    y_train_ds: InputDataset,
+    model_artifact: OutputModel
+):
+    
+    import joblib
+    import numpy as np
+    from sklearn.neural_network import MLPClassifier
+    from sklearn.svm import LinearSVC
+    from sklearn.ensemble import RandomForestClassifier
+
+
+    # Load data
+    x_train = np.load(x_train_ds.path)
+    y_train = np.load(y_train_ds.path)
+
+    # Train model
+    model = LinearSVC(random_state=42, max_iter=1000)
+    model.fit(x_train, y_train)
+
+    # Save model
+    joblib.dump(model, model_artifact.path)
+    print(f"Model saved to {model_artifact.path}")
