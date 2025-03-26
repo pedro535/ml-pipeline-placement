48b3080a46333597b6bef8595e584033b58f163a
Pedro Rodrigues <pedrofrodrigues4@gmail.com>
2025-03-26 22:26:34 +0000

    Add custom pipelines for kddcup99

---
+++ pipelines/kddcup99_SVM/model_evaluation.py
@@ -0,0 +1,30 @@
+from mlopx.artifacts import InputModel, InputDataset
+from typing import Dict
+
+
+def model_evaluation(
+    model_artifact: InputModel,
+    x_test_ds: InputDataset,
+    y_test_ds: InputDataset
+) -> Dict[str, float]:
+
+    import joblib
+    import numpy as np
+    from sklearn.metrics import accuracy_score, f1_score, precision_score
+
+    # Load data
+    X_test = np.load(x_test_ds.path)
+    y_test = np.load(y_test_ds.path)
+
+    # Load model
+    model = joblib.load(model_artifact.path)
+
+    # Evaluate model
+    y_pred = model.predict(X_test)
+    
+    metrics = {
+        "accuracy": accuracy_score(y_test, y_pred),
+        "f1_score": f1_score(y_test, y_pred),
+        "precision": precision_score(y_test, y_pred)
+    }
+    return metrics
---
+++ pipelines/kddcup99_SVM/pipeline.py
@@ -0,0 +1,36 @@
+from mlopx import Pipeline, Component
+
+from data_preprocessing import data_preprocessing
+from model_training import model_training
+from model_evaluation import model_evaluation
+
+BASE_IMAGE = "registry.localhost/kfp_python_base:v1"
+
+component_1 = Component(
+    func=data_preprocessing,
+    image=BASE_IMAGE,
+    args={
+        "dataset_path": "/mnt/datasets/kddcup99/kddcup.data_10_percent"
+    }
+)
+
+component_1.mount_volume(
+    pvc="datasets-pvc",
+    mount_path="/mnt/datasets"
+)
+
+component_2 = Component(
+    image=BASE_IMAGE, 
+    func=model_training,
+    args={}
+)
+
+component_3 = Component(
+    image=BASE_IMAGE,
+    func=model_evaluation,
+    args={}
+)
+
+pipeline = Pipeline(name="kddcup99", metadata_file="metadata.json")
+pipeline.add([component_1, component_2, component_3])
+pipeline.submit("http://127.0.0.1:8000")
