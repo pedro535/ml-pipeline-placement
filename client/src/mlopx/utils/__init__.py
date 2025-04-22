33b0924c8401d62776ce9ae54c9695cc2664050e
Pedro Rodrigues <pedrofrodrigues4@gmail.com>
2025-04-22 15:53:28 +0100

    Add client utils with dnn model util class

---
+++ client/src/mlopx/utils/__init__.py
@@ -0,0 +1,1 @@
+from .model_details import ModelDetails
---
+++ client/src/mlopx/utils/model_details.py
@@ -0,0 +1,30 @@
+from typing import List
+import keras
+import json
+
+
+class ModelDetails:
+
+    @staticmethod
+    def tf_dnn_layers(model: keras.Model) -> List:
+        """
+        Return layer details of a TensorFlow DNN model.
+        """
+        layers = []
+        for layer in model.layers:
+            details = {
+                "type": layer.__class__.__name__,
+                "input_shape": layer.input.shape[1:],
+                "output_shape": layer.output.shape[1:],
+                "params": layer.count_params()
+            }
+
+            if isinstance(layer, keras.layers.Conv2D):
+                details["kernel_size"] = layer.kernel_size
+                details["strides"] = layer.strides
+            elif isinstance(layer, keras.layers.MaxPooling2D):
+                details["pool_size"] = layer.pool_size
+            
+            layers.append(details)
+            print(json.dumps(details, indent=4))
+        return layers
