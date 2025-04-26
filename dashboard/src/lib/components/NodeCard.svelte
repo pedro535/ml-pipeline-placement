05099387fd49f3556854f70a7fdf8acda8728673
Pedro Rodrigues <pedrofrodrigues4@gmail.com>
2025-04-26 03:08:02 +0100

    Add nodes and datasets to dashboard

--- dashboard/src/lib/components/NodeCard.svelte
+++ dashboard/src/lib/components/NodeCard.svelte
@@ -1,16 +1,8 @@
 <script>
-    export let node_details = {
-        name: "",
-        status: "",
-        cpu: 0,
-        architecture: "",
-        os: "",
-        os_version: "",
-        memory: 0,
-        gpu: false,
-        running_pipeline: false,
-        waiting_pipelines: 0
-    };
+    import { Badge } from 'flowbite-svelte';
+
+    export let node_details = undefined;
+    export let n_assignments = 0;
 </script>
 
 <div class="w-full p-5 mb-5 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
@@ -19,12 +11,17 @@
 
     <div class="mb-1 mb">
         <span class="text-black dark:text-gray-400 font-semibold text-sm">Status: </span>
-        <span class="font-light text-gray-500 dark:text-gray-400 text-sm">{node_details.status}</span>
+        <Badge class="ml-1" color="green">Running</Badge>
+    </div>
+
+    <div>
+        <span class="text-black dark:text-gray-400 font-semibold text-sm">Tasks Assigned: </span>
+        <Badge class="ml-1" color="purple">{n_assignments}</Badge>
     </div>
 
     <div class="mb-1">
         <span class="text-black dark:text-gray-400 font-semibold text-sm">CPU: </span>
-        <span class="font-light text-gray-500 dark:text-gray-400 text-sm">{node_details.cpu} cores</span>
+        <span class="font-light text-gray-500 dark:text-gray-400 text-sm">{node_details.cpu_cores} cores</span>
     </div>
 
     <div class="mb-1">
@@ -33,32 +30,23 @@
     </div>
 
     <div>
+        <span class="text-black dark:text-gray-400 font-semibold text-sm">Memory: </span>
+        <span class="font-light text-gray-500 dark:text-gray-400 text-sm">{Math.round(node_details.memory / 1000000)} GB</span>
+    </div>
+
+    <div>
+        <span class="text-black dark:text-gray-400 font-semibold text-sm">Accelerator: </span>
+        <span class="font-light text-gray-500 dark:text-gray-400 text-sm">{node_details.accelerator}</span>
+    </div>
+
+    <div>
         <span class="text-black dark:text-gray-400 font-semibold text-sm">OS: </span>
         <span class="font-light text-gray-500 dark:text-gray-400 text-sm">{node_details.os}</span>
     </div>
 
     <div>
-        <span class="text-black dark:text-gray-400 font-semibold text-sm">OS Version: </span>
-        <span class="font-light text-gray-500 dark:text-gray-400 text-sm">{node_details.os_version}</span>
+        <span class="text-black dark:text-gray-400 font-semibold text-sm">OS Image: </span>
+        <span class="font-light text-gray-500 dark:text-gray-400 text-sm">{node_details.os_image}</span>
     </div>
 
-    <div>
-        <span class="text-black dark:text-gray-400 font-semibold text-sm">Memory: </span>
-        <span class="font-light text-gray-500 dark:text-gray-400 text-sm">{node_details.memory} GB</span>
-    </div>
-
-    <div>
-        <span class="text-black dark:text-gray-400 font-semibold text-sm">GPU: </span>
-        <span class="font-light text-gray-500 dark:text-gray-400 text-sm">{node_details.gpu ? "Yes" : "No"}</span>
-    </div>
-
-    <div>
-        <span class="text-black dark:text-gray-400 font-semibold text-sm">Running Pipeline: </span>
-        <span class="font-light text-gray-500 dark:text-gray-400 text-sm">{node_details.running_pipeline ? "Yes" : "No"}</span>
-    </div>
-
-    <div>
-        <span class="text-black dark:text-gray-400 font-semibold text-sm">Pipelines waiting: </span>
-        <span class="font-light text-gray-500 dark:text-gray-400 text-sm">{node_details.waiting_pipelines}</span>
-    </div>
 </div>
--- dashboard/src/routes/nodes/+page.svelte
+++ dashboard/src/routes/nodes/+page.svelte
@@ -1,33 +1,37 @@
 <script>
-    import { Card } from 'flowbite-svelte';
+    import { onMount } from "svelte";
     import NodeCard from '$lib/components/NodeCard.svelte';
+    import { Spinner } from 'flowbite-svelte';
 
-    let node1 = {
-        name: "ml-worker-high-cpu-01",
-        status: "running",
-        cpu: 8,
-        architecture: "x86_64",
-        os: "Linux",
-        os_version: "Ubuntu 20.04",
-        memory: 32,
-        gpu: true,
-        running_pipeline: true,
-        waiting_pipelines: 4
-    };
+    let nodes = [];
+    let assignments = {};
+    let loading = true;
+
+    onMount(async () => {
+        try {
+            const response = await fetch("http://127.0.0.1:8000/nodes/");
+            const data = await response.json();
+            nodes = data.nodes;
+            assignments = data.assignments;
+            loading = false;
+        } catch (error) {
+            console.log(error);
+            loading = true;
+        }
+    });
 </script>
 
 <div>
-    <h1 class="text-4xl font-bold mb-10">
-        Worker Nodes
-    </h1>
-
-    <div class="grid lg:grid-cols-3 grid-cols-1 gap-5">
-        <NodeCard node_details={node1} />
-        <NodeCard node_details={node1} />
-        <NodeCard node_details={node1} />
-        <NodeCard node_details={node1} />
-        <NodeCard node_details={node1} />
-        <NodeCard node_details={node1} />
-        <NodeCard node_details={node1} />
-    </div>
+    <h1 class="text-4xl font-bold mb-10">Worker Nodes</h1>
+    {#if loading}
+        <div class="text-center mt-5">
+            <Spinner size={10} />
+        </div>
+    {:else}
+        <div class="grid lg:grid-cols-3 grid-cols-1 gap-5">
+            {#each nodes as node}
+                <NodeCard node_details={node} n_assignments={assignments[node.name]} />
+            {/each}
+        </div>
+    {/if}
 </div>
